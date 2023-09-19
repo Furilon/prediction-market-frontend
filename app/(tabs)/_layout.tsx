03389b80e80b927f2a2eaa389b93906e4b616452
Mykyta Medvediev <arlekin8881@gmail.com>
2023-09-19 10:32:27 -0700

    Removed the tab headers and added tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,38 +1,48 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Link, Tabs } from "expo-router";
-import { Pressable, useColorScheme } from "react-native";
+import { Tabs } from "expo-router";
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>["name"];
-  color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return (
+    <FontAwesome
+      size={28}
+      color="white"
+      style={{ marginBottom: -3 }}
+      {...props}
+    />
+  );
 }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
+          tabBarIcon: () => <TabBarIcon name="home" />,
         }}
       />
       <Tabs.Screen
         name="questions"
         options={{
           title: "Questions",
+          tabBarIcon: () => <TabBarIcon name="newspaper-o" />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profile",
+          tabBarIcon: () => <TabBarIcon name="user-o" />,
         }}
       />
     </Tabs>
