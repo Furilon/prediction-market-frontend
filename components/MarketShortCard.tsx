99bad50f3466e24f63336f778a585a1d4a2bddcd
Mykyta Medvediev <arlekin8881@gmail.com>
2023-09-20 19:31:18 -0700

    Styled the market cards on the questions page

--- components/MarketShortCard.tsx
+++ components/MarketShortCard.tsx
@@ -1,17 +1,46 @@
 import { ScrollView, View, StyleSheet } from "react-native";
 import { Card, Text, Avatar, IconButton } from "react-native-paper";
 import { MarketShortCardProps } from "../types/markets";
+import { router } from "expo-router";
 
 export default function MarketShortCard(
   props: MarketShortCardProps
 ): JSX.Element {
   return (
-    <Card>
+    <Card onPress={() => router.push(`/market?id=${props.id}`)}>
       <Card.Content>
-        <Avatar.Icon size={24} icon="account" />
-        <Text>{props.question}</Text>
-        <Text>{props.price}%</Text>
-        <Text>{props.numberOfTraders}</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          <Avatar.Icon size={24} icon="account" />
+          <Text variant="bodyLarge" style={{ marginLeft: 10 }}>
+            {props.question}
+          </Text>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "flex-end",
+            alignItems: "center",
+          }}
+        >
+          <Text>{props.price}%</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+          >
+            <IconButton disabled icon="account" size={24} />
+            <Text style={{ marginLeft: -10 }}>{props.numberOfTraders}</Text>
+          </View>
+        </View>
       </Card.Content>
     </Card>
   );
