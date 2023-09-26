e1120a93e4ff902ff2e1a963b304bccdd313e6c7
Mykyta Medvediev <arlekin8881@gmail.com>
2023-09-26 14:11:07 -0700

    Rebuilt register util using await/async

--- utils/register.ts
+++ utils/register.ts
@@ -2,13 +2,27 @@ import { UserRegisterInfo } from "../types/auth";
 import setToken from "./setToken";
 
 export default async function register(payload: UserRegisterInfo) {
-  fetch("http://192.168.56.1:8080/auth/register", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => response.json())
-    .then((jsonObj) => setToken(jsonObj));
+  try {
+    const response = await fetch("http://192.168.56.1:8080/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "*/*",
+        Connection: "keep-alive",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error registering user");
+    }
+
+    const jsonObj = await response.json();
+    await setToken(jsonObj);
+
+    return jsonObj;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
