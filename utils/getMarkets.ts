15e2edd573cf5b5db1fa9e246438d94c37f2d7c8
Mykyta Medvediev <arlekin8881@gmail.com>
2023-09-26 14:10:39 -0700

    Removed annoying TS error

--- utils/getMarkets.ts
+++ utils/getMarkets.ts
@@ -1,7 +1,7 @@
 import getToken from "./getToken";
 
 export default async function getMarkets() {
-  const { token } = await getToken();
+  const { token }: any = await getToken();
 
   const response = await fetch("http://192.168.56.1:8080/markets", {
     method: "GET",
