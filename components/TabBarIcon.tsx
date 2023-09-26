76383e095fa1f05fc3ecc902e6dec80ddb5b778c
Mykyta Medvediev <arlekin8881@gmail.com>
2023-09-25 22:05:48 -0700

    Refactored TabBarIcon

---
+++ components/TabBarIcon.tsx
@@ -0,0 +1,14 @@
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>["name"];
+}) {
+  return (
+    <FontAwesome
+      size={28}
+      color="white"
+      style={{ marginBottom: -3 }}
+      {...props}
+    />
+  );
+}
