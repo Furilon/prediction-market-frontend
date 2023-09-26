022d8bc9b325bb79c05e1cc86d51aef6445884b9
Mykyta Medvediev <arlekin8881@gmail.com>
2023-09-26 12:09:36 -0700

    Removed the header

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function StackLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="login" />
       <Stack.Screen name="register" />
     </Stack>
