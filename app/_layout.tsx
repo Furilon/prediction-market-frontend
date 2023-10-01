224c7ac10fa090a88e0556db46cf0c236029bc28
Mykyta Medvediev <arlekin8881@gmail.com>
2023-09-30 19:38:05 -0700

    Refactored the auth logic into the AuthContext

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,16 +6,10 @@ import {
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
-import { useEffect, useReducer, useState, useMemo, createContext } from "react";
+import { useEffect } from "react";
 import { useColorScheme } from "react-native";
 import { PaperProvider } from "react-native-paper";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import authenticate from "../utils/authenticate";
-import { UserAuthInfo, UserRegisterInfo } from "../types/auth";
-import setToken from "../utils/setToken";
-import register from "../utils/register";
-import { AuthContext } from "../context/AuthContext";
-import getMarkets from "../utils/getMarkets";
+import { SessionProvider } from "../context/AuthContext";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -51,128 +45,16 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
-  const [state, dispatch] = useReducer(reducer, {
-    isLoading: true,
-    isSignout: false,
-    userToken: null,
-  });
-
-  const authContext = useMemo(
-    () => ({
-      signIn: async (data: UserAuthInfo) => {
-        let token;
-        try {
-          token = authenticate(data);
-          console.log(
-            "Authenticated and received the following token: " + token
-          );
-        } catch (e: any) {
-          console.log(e.message);
-          return;
-        }
-
-        dispatch({ type: "SIGN_IN", token });
-      },
-      signOut: () => {
-        console.log("Signed out");
-        dispatch({ type: "SIGN_OUT" });
-      },
-      signUp: async (data: UserRegisterInfo) => {
-        let token;
-        try {
-          token = register(data);
-          console.log("Registered and received the following token: " + token);
-        } catch (e: any) {
-          console.log(e.message);
-          return;
-        }
-
-        dispatch({ type: "SIGN_IN", token });
-      },
-    }),
-    []
-  );
-
-  useEffect(() => {
-    const bootstrapAsync = async () => {
-      let jwtToken;
-      let markets;
-
-      try {
-        jwtToken = await AsyncStorage.getItem("token");
-        console.log(jwtToken);
-        console.log(typeof jwtToken);
-      } catch (e: any) {
-        console.log("Restroing token failed");
-        console.log(e.message);
-        console.log("Logging out");
-        authContext.signOut();
-        console.log(state.userToken);
-        return;
-      }
-
-      if (jwtToken == null) {
-        authContext.signOut();
-        return;
-      }
-
-      // After restoring token, we may need to validate it in production apps
-      try {
-        markets = await getMarkets(jwtToken as string);
-        console.log(markets);
-      } catch (e: any) {
-        console.log("Token is invalid");
-        console.log(e.message);
-        console.log("Logging out");
-        authContext.signOut();
-        console.log(state.userToken);
-        return;
-      }
-
-      // This will switch to the App screen or Auth screen and this loading
-      // screen will be unmounted and thrown away.
-      // dispatch({ type: "RESTORE_TOKEN", token: jwtToken });
-    };
-
-    bootstrapAsync();
-  }, []);
 
   return (
     <PaperProvider>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <AuthContext.Provider value={authContext}>
+        <SessionProvider>
           <Stack>
-            {state.userToken == null ? (
-              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            ) : (
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            )}
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           </Stack>
-        </AuthContext.Provider>
+        </SessionProvider>
       </ThemeProvider>
     </PaperProvider>
   );
-}
-
-function reducer(prevState: any, action: any) {
-  switch (action.type) {
-    case "RESTORE_TOKEN":
-      return {
-        ...prevState,
-        userToken: action.token,
-        isLoading: false,
-      };
-    case "SIGN_IN":
-      return {
-        ...prevState,
-        isSignout: false,
-        userToken: action.token,
-      };
-    case "SIGN_OUT":
-      return {
-        ...prevState,
-        isSignout: true,
-        userToken: null,
-      };
-  }
 }
--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,8 +1,62 @@
-import { createContext } from "react";
-import { AuthContextType } from "../types/auth";
+import React from "react";
+import { useStorageState } from "../hooks/useStorageState";
+import { UserAuthInfo, UserRegisterInfo } from "../types/auth";
+import authenticate from "../utils/authenticate";
+import register from "../utils/register";
 
-export const AuthContext = createContext<AuthContextType>({
-  signIn: () => {},
-  signOut: () => {},
-  signUp: () => {},
-});
+const AuthContext = React.createContext<{
+  signIn: ({ username, password }: UserAuthInfo) => void;
+  signOut: () => void;
+  signUp: ({
+    username,
+    password,
+    firstName,
+    lastName,
+  }: UserRegisterInfo) => void;
+  session?: string | null;
+  isLoading: boolean;
+} | null>(null);
+
+// This hook can be used to access the user info.
+export function useSession() {
+  const value = React.useContext(AuthContext);
+  if (process.env.NODE_ENV !== "production") {
+    if (!value) {
+      throw new Error("useSession must be wrapped in a <SessionProvider />");
+    }
+  }
+
+  return value;
+}
+
+export function SessionProvider(props: any) {
+  const [[isLoading, session], setSession] = useStorageState("token");
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn: ({ username, password }: UserAuthInfo) => {
+          // Perform sign-in logic here
+
+          setSession("xxx");
+        },
+        signOut: () => {
+          setSession(null);
+        },
+        signUp: ({
+          username,
+          password,
+          firstName,
+          lastName,
+        }: UserRegisterInfo) => {
+          // Perform sign-up logic here
+          setSession("xxx");
+        },
+        session,
+        isLoading,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
